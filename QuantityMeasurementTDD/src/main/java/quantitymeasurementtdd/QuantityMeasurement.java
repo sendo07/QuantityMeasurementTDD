4f422e1fa9a8ce84495dbe238c286992e1ee104b
sendo07 <loakesh.i@gmail.com>
2020-09-01 04:44:50 +0530

    Refactore accessmodifiers in code

--- QuantityMeasurementTDD/src/main/java/quantitymeasurementtdd/QuantityMeasurement.java
+++ QuantityMeasurementTDD/src/main/java/quantitymeasurementtdd/QuantityMeasurement.java
@@ -4,6 +4,6 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class QuantityMeasurement {
-    BiFunction<Double, Double, Boolean> compareQuantity = (feet, inches) -> this.feetToInches.apply(feet).equals(inches);
-    Function<Double, Double> feetToInches = feet -> feet * 12;
+    public BiFunction<Double, Double, Boolean> compareQuantity = (feet, inches) -> this.feetToInches.apply(feet).equals(inches);
+    private Function<Double, Double> feetToInches = feet -> feet * 12;
 }
