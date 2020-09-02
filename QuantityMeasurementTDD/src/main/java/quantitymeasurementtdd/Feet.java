0265702502013ba9ab1e5b1f1398cdf35f133a15
sendo07 <loakesh.i@gmail.com>
2020-09-02 11:22:53 +0530

    Added Code for checking Feet & Inch Values

---
+++ QuantityMeasurementTDD/src/main/java/quantitymeasurementtdd/Feet.java
@@ -0,0 +1,18 @@
+package quantitymeasurementtdd;
+
+public class Feet {
+
+    private double value;
+
+    public Feet(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.value, value) == 0;
+    }
+}
