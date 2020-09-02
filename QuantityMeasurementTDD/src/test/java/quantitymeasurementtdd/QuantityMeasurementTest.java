e075278b5294edc2e3e5361d5726d96d5f5cfe29
sendo07 <loakesh.i@gmail.com>
2020-09-02 11:22:16 +0530

    Added Test Code for checking Feet & Inch Values

--- QuantityMeasurementTDD/src/test/java/quantitymeasurementtdd/QuantityMeasurementTest.java
+++ QuantityMeasurementTDD/src/test/java/quantitymeasurementtdd/QuantityMeasurementTest.java
@@ -29,4 +29,32 @@ public class QuantityMeasurementTest {
         boolean result = quantityMeasurement.compareQuantity.apply(1.00, 10.00);
         Assert.assertFalse(result);
     }
+
+    @Test
+    public void given0Feetand0Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0Feetand1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0Inchand0Inch_ShouldReturnEquals() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1, inch2);
+    }
+
+    @Test
+    public void given0Inchand1Inch_ShouldReturnNotEquals() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1, inch2);
+    }
 }
